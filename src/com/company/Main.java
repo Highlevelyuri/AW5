f83b19f27c4401a0c1ea2f3b2bf8f1f7593c9745
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-03 14:57:08 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for ( int x =1 ; x<40; x+=4 ){
+	    int sum = 0;
+	    sum +=x;
+        System.out.println(sum);
+    }
+
+    }
+}
